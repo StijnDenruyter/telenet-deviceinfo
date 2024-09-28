ade8aa6e9ef31a8587d9bf3f1fb8a4cf7c747647
Stijn Denruyter <stijn@denruyter.net>
2024-09-28 22:49:25 +0200

    Version 1.0

---
+++ Main.vb
@@ -0,0 +1,15 @@
+﻿Module Main
+    Private DeviceInfoIcon As NotifyIcon
+    Sub Main()
+        DeviceInfoIcon = New NotifyIcon
+        DeviceInfoIcon.Icon = New Icon("Icon.ico")
+        GetDeviceInfo()
+        DeviceInfoIcon.Visible = True
+        Application.Run()
+    End Sub
+    Private Sub GetDeviceInfo()
+        Dim Hostname As String = Environment.MachineName
+        Dim Username As String = Environment.UserName
+        DeviceInfoIcon.Text = "Hostname: " & Hostname & vbCrLf & "Username: " & Username
+    End Sub
+End Module
